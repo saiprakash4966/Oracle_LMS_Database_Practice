7051e6d088dc3a59c412926093c375bb4b8ec610
saiprakash4966 <97298373+saiprakash4966@users.noreply.github.com>
2022-08-28 20:40:31 +0530

    Update CandidateDocuments.sql

--- CandidateDocuments.sql
+++ CandidateDocuments.sql
@@ -1,38 +1,50 @@
 
 
-mysql> create table Candidate_Document(id int PRIMARY KEY,candidate_id int not null,doc_type VARCHAR(50) not null,doc_path blob,status VARCHAR(50),creator_stamp datetime,creator_user int not null);
-Query OK, 0 rows affected (0.05 sec)
+mysql> CREATE TABLE candidate_documents
+    -> (id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ->        candidate_id INT(10) NOT NULL,
+    ->        doc_type VARCHAR(100),
+    ->        doc_path VARCHAR(2048),
+    ->        status ENUM('pending', 'received'),
+    ->        creator_stamp DATETIME NOT NULL,
+    ->        creator_user VARCHAR(100),
+    ->        FOREIGN KEY (candidate_id)
+    ->        REFERENCES fellowship_candidate(id)
+    ->        ON UPDATE CASCADE
+    ->        ON DELETE CASCADE
+    ->      );
 
-mysql> desc Candidate_Document;
-+---------------+-------------+------+-----+---------+-------+
-| Field         | Type        | Null | Key | Default | Extra |
-+---------------+-------------+------+-----+---------+-------+
-| id            | int         | NO   | PRI | NULL    |       |
-| candidate_id  | int         | NO   |     | NULL    |       |
-| doc_type      | varchar(50) | NO   |     | NULL    |       |
-| doc_path      | blob        | YES  |     | NULL    |       |
-| status        | varchar(50) | YES  |     | NULL    |       |
-| creator_stamp | datetime    | YES  |     | NULL    |       |
-| creator_user  | int         | NO   |     | NULL    |       |
-+---------------+-------------+------+-----+---------+-------+
-7 rows in set (0.00 sec)
 
-mysql> insert into candidate_document values(1,0101,'AAdhar','www.sai.com','Received','22-01-20 01:05:09',1);
+mysql> INSERT INTO candidate_documents VALUES(1, 1, "PAN NO", "https://docs.google.com/document/pan", "received", "24-02-22 13:34:25", "sai");
+Query OK, 1 row affected (0.01 sec)
 
-mysql> insert into candidate_document values(2,0102,'AAdhar','www.manu.com','Received','23-01-20 02:05:09',2),
-(3,0103,'AAdhar','www.manu.com','Received','23-01-20 03:05:09',3),
-(4,0104,'AAdhar','www.pdf.com','Received','24-01-20 04:05:09',4),
-(5,0105,'AAdhar','www.word.com','Received','25-01-20 05:05:09',5);
+mysql>
+mysql> INSERT INTO candidate_documents VALUES(2, 2, "ADHAR NO", "https://docs.google.com/document/aadhar", "received", "14-03-20 13:34:25", "manoj");
+Query OK, 1 row affected (0.01 sec)
 
-mysql> select * from candidate_document;
-+----+--------------+----------+----------------------------+----------+---------------------+--------------+
-| id | candidate_id | doc_type | doc_path                   | status   | creator_stamp       | creator_user |
-+----+--------------+----------+----------------------------+----------+---------------------+--------------+
-|  1 |          101 | AAdhar   | 0x7777772E7361692E636F6D   | Received | 2022-01-20 01:05:09 |            1 |
-|  2 |          102 | AAdhar   | 0x7777772E6D616E752E636F6D | Received | 2023-01-20 02:05:09 |            2 |
-|  3 |          103 | AAdhar   | 0x7777772E6D616E752E636F6D | Received | 2023-01-20 03:05:09 |            3 |
-|  4 |          104 | AAdhar   | 0x7777772E7064662E636F6D   | Received | 2024-01-20 04:05:09 |            4 |
-|  5 |          105 | AAdhar   | 0x7777772E776F72642E636F6D | Received | 2025-01-20 05:05:09 |            5 |
-+----+--------------+----------+----------------------------+----------+---------------------+--------------+
+mysql>
+mysql> INSERT INTO candidate_documents VALUES(3, 3, "PAN NO", "https://docs.google.com/document/pan", "received", "16-07-21 15:36:05", "saiprakash");
+Query OK, 1 row affected (0.00 sec)
+
+mysql>
+mysql> INSERT INTO candidate_documents VALUES(4, 4, "ADHAR NO", "https://docs.google.com/document/aadhar", "received", "18-03-22 12:52:15", "raghu");
+Query OK, 1 row affected (0.00 sec)
+
+mysql>
+mysql> INSERT INTO candidate_documents VALUES(5, 5, "AADHAR NO", "https://docs.google.com/document/aadhar", "received", "19-08-21 14:03:25", "anji");
+Query OK, 1 row affected (0.00 sec)
+
+
+
+mysql> select * from candidate_documents;
++----+--------------+-----------+-----------------------------------------+----------+---------------------+--------------+
+| id | candidate_id | doc_type  | doc_path                                | status   | creator_stamp       | creator_user |
++----+--------------+-----------+-----------------------------------------+----------+---------------------+--------------+
+|  1 |            1 | PAN NO    | https://docs.google.com/document/pan    | received | 2024-02-22 13:34:25 | sai          |
+|  2 |            2 | ADHAR NO  | https://docs.google.com/document/aadhar | received | 2014-03-20 13:34:25 | manoj        |
+|  3 |            3 | PAN NO    | https://docs.google.com/document/pan    | received | 2016-07-21 15:36:05 | saiprakash   |
+|  4 |            4 | ADHAR NO  | https://docs.google.com/document/aadhar | received | 2018-03-22 12:52:15 | raghu        |
+|  5 |            5 | AADHAR NO | https://docs.google.com/document/aadhar | received | 2019-08-21 14:03:25 | anji         |
++----+--------------+-----------+-----------------------------------------+----------+---------------------+--------------+
 5 rows in set (0.00 sec)
 
