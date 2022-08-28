c0afd8ec6e984055d5770c733382f7c7e46ff21c
saiprakash4966 <97298373+saiprakash4966@users.noreply.github.com>
2022-08-28 22:53:19 +0530

    Update LabThreshold.sql

--- LabThreshold.sql
+++ LabThreshold.sql
@@ -1,39 +1,52 @@
 
 
-mysql> create table LabThreshold(Id int PRIMARY KEY,Lab_Id int not null,Lab_Capacity int not null,Lead_Threshold int not null,Ideation_engg_Threshold int not null,buddy_engg_threshold_status VARCHAR(100),creator_stamp datetime,creator_user int not null);
-Query OK, 0 rows affected (0.07 sec)
-
-mysql> desc LabThreshold;
-+-----------------------------+--------------+------+-----+---------+-------+
-| Field                       | Type         | Null | Key | Default | Extra |
-+-----------------------------+--------------+------+-----+---------+-------+
-| Id                          | int          | NO   | PRI | NULL    |       |
-| Lab_Id                      | int          | NO   |     | NULL    |       |
-| Lab_Capacity                | int          | NO   |     | NULL    |       |
-| Lead_Threshold              | int          | NO   |     | NULL    |       |
-| Ideation_engg_Threshold     | int          | NO   |     | NULL    |       |
-| buddy_engg_threshold_status | varchar(100) | YES  |     | NULL    |       |
-| creator_stamp               | datetime     | YES  |     | NULL    |       |
-| creator_user                | int          | NO   |     | NULL    |       |
-+-----------------------------+--------------+------+-----+---------+-------+
-8 rows in set (0.00 sec)
-
-
-mysql> insert into  LabThreshold values(1,20,200,10,50,'active','09-09-08 02:00:03',1),
-(2,40,150,20,70,'active','19-09-08 02:00:03',2),
-(3,26,100,30,90,'active','29-09-08 02:00:03',3),
-(4,80,500,40,20,'active','11-09-08 02:00:03',4),
-(5,50,60,85,40,'active','08-09-08 02:00:03',5);
+mysql> CREATE TABLE lab_threshold
+    ->      ( id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ->        lab_Id INT(10),
+    ->        lab_capacity INT,
+    ->        lead_threshold INT,
+    ->        ideation_engg_threshold INT,
+    ->        buddy_engg_threshold INT,
+    ->        status ENUM('active', 'inactive'),
+    ->        creator_stamp DATETIME NOT NULL,
+    ->        creator_user VARCHAR(100),
+    ->        FOREIGN KEY (lab_id)
+    ->        REFERENCES lab(id)
+    ->        ON UPDATE CASCADE
+    ->        ON DELETE CASCADE
+    ->      );
+Query OK, 0 rows affected, 2 warnings (0.06 sec)
 
 
-mysql> select * from LabThreshold;
-+----+--------+--------------+----------------+-------------------------+-----------------------------+---------------------+--------------+
-| Id | Lab_Id | Lab_Capacity | Lead_Threshold | Ideation_engg_Threshold | buddy_engg_threshold_status | creator_stamp       | creator_user |
-+----+--------+--------------+----------------+-------------------------+-----------------------------+---------------------+--------------+
-|  1 |     20 |          200 |             10 |                      50 | active                      | 2009-09-08 02:00:03 |            1 |
-|  2 |     40 |          150 |             20 |                      70 | active                      | 2019-09-08 02:00:03 |            2 |
-|  3 |     26 |          100 |             30 |                      90 | active                      | 2029-09-08 02:00:03 |            3 |
-|  4 |     80 |          500 |             40 |                      20 | active                      | 2011-09-08 02:00:03 |            4 |
-|  5 |     50 |           60 |             85 |                      40 | active                      | 2008-09-08 02:00:03 |            5 |
-+----+--------+--------------+----------------+-------------------------+-----------------------------+---------------------+--------------+
+
+
+mysql> INSERT INTO lab_threshold value(1, 1, 100, 5, 23, 5, 'Active', '23-05-13 10:23:45','sai');
+Query OK, 1 row affected (0.02 sec)
+
+mysql>
+mysql> INSERT INTO lab_threshold value(2, 2, 50, 10, 21, 2, 'InActive', '13-05-18 10:43:15','prakash');
+Query OK, 1 row affected (0.01 sec)
+
+mysql>
+mysql> INSERT INTO lab_threshold value(3, 3, 150, 20, 15, 2, 'InActive', '03-05-19 10:20:03','vinay');
+Query OK, 1 row affected (0.01 sec)
+
+mysql>
+mysql> INSERT INTO lab_threshold value(4, 4, 150, 20, 15, 2, 'InActive', '03-05-19 10:20:03','manoj');
+Query OK, 1 row affected (0.01 sec)
+
+mysql>
+mysql> INSERT INTO lab_threshold value(5, 5, 150, 20, 15, 2, 'InActive', '03-05-19 10:20:03','raghu');
+Query OK, 1 row affected (0.01 sec)
+
+mysql> select * from lab_threshold;
++----+--------+--------------+----------------+-------------------------+----------------------+----------+---------------------+--------------+
+| id | lab_Id | lab_capacity | lead_threshold | ideation_engg_threshold | buddy_engg_threshold | status   | creator_stamp       | creator_user |
++----+--------+--------------+----------------+-------------------------+----------------------+----------+---------------------+--------------+
+|  1 |      1 |          100 |              5 |                      23 |                    5 | active   | 2023-05-13 10:23:45 | sai          |
+|  2 |      2 |           50 |             10 |                      21 |                    2 | inactive | 2013-05-18 10:43:15 | prakash      |
+|  3 |      3 |          150 |             20 |                      15 |                    2 | inactive | 2003-05-19 10:20:03 | vinay        |
+|  4 |      4 |          150 |             20 |                      15 |                    2 | inactive | 2003-05-19 10:20:03 | vinay        |
+|  5 |      5 |          150 |             20 |                      15 |                    2 | inactive | 2003-05-19 10:20:03 | vinay        |
++----+--------+--------------+----------------+-------------------------+----------------------+----------+---------------------+--------------+
 5 rows in set (0.00 sec)
