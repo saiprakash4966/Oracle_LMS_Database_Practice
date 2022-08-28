f28715796713017e85fa28e396bfa7cf2aea62ca
saiprakash4966 <97298373+saiprakash4966@users.noreply.github.com>
2022-08-28 23:20:09 +0530

    Update StackAssignment.sql

--- StackAssignment.sql
+++ StackAssignment.sql
@@ -1,27 +1,44 @@
 
 
-mysql> create table Stack_Assignment(Id int PRIMARY KEY,Requirement_Id int not null,Candidate_Id int not null,assign_date datetime,complte_date datetime,status VARCHAR(50),creator_stamp datetime,creator_user int not null);
-Query OK, 0 rows affected (0.05 sec)
+mysql> CREATE TABLE candidate_stack_assignment
+    ->     ( id INT(10) NOT NULL PRIMARY KEY,
+    ->       requirement_id INT(10),
+    ->       candidate_id INT(10),
+    ->       assigned_date DATE,
+    ->       complete_date DATE,
+    ->      status ENUM('active', 'inactive'),
+    ->       creator_stamp DATETIME NOT NULL,
+    ->       creator_user VARCHAR(100),
+    ->        FOREIGN KEY (requirement_id) REFERENCES company_requirement(id)
+    ->        ON UPDATE CASCADE ON DELETE CASCADE,
+    ->        FOREIGN KEY (candidate_id) REFERENCES fellowship_candidate(Id)
+    ->    ON UPDATE CASCADE ON DELETE CASCADE
+    ->     );
+Query OK, 0 rows affected, 3 warnings (0.10 sec)
 
+mysql> INSERT into candidate_stack_assignment values(1,1,1,'01-01-19','01-01-20','active','03-04-05 01:00:02','saI');
+Query OK, 1 row affected (0.03 sec)
 
-mysql> desc Stack_Assignment;
-+----------------+-------------+------+-----+---------+-------+
-| Field          | Type        | Null | Key | Default | Extra |
-+----------------+-------------+------+-----+---------+-------+
-| Id             | int         | NO   | PRI | NULL    |       |
-| Requirement_Id | int         | NO   |     | NULL    |       |
-| Candidate_Id   | int         | NO   |     | NULL    |       |
-| assign_date    | datetime    | YES  |     | NULL    |       |
-| complte_date   | datetime    | YES  |     | NULL    |       |
-| status         | varchar(50) | YES  |     | NULL    |       |
-| creator_stamp  | datetime    | YES  |     | NULL    |       |
-| creator_user   | int         | NO   |     | NULL    |       |
-+----------------+-------------+------+-----+---------+-------+
-8 rows in set (0.00 sec)
+mysql> INSERT into candidate_stack_assignment values(2,2,2,'02-01-19','02-01-20','active','05-04-05 01:00:02','manoj');
+Query OK, 1 row affected (0.01 sec)
 
+mysql> INSERT into candidate_stack_assignment values(3,3,3,'03-01-19','03-01-20','active','06-04-05 01:00:02','vijay');
+Query OK, 1 row affected (0.01 sec)
 
-mysql> insert into stack_assignment values(1,1,1,'01-01-19 06:05:00','done','03-04-05 01:00:02',1),
-(2,2,2,'02-01-19 07:05:00','done','05-04-05 01:00:02',2),
-(3,3,3,'03-01-19 08:05:00','done','06-04-05 01:00:02',3),
-(4,4,4,'04-01-19 09:05:00','done','07-04-05 01:00:02',4),
-(5,5,5,'05-01-19 01:05:00','done','08-04-05 01:00:02',5);
+mysql> INSERT into candidate_stack_assignment values(4,4,4,'04-01-19','04-01-20','active','07-04-05 01:00:02','nithin');
+Query OK, 1 row affected (0.01 sec)
+
+mysql> INSERT into candidate_stack_assignment values(5,5,5,'05-01-19','05-01-20','active','08-04-05 01:00:02','vamsi');
+Query OK, 1 row affected (0.01 sec)
+
+mysql> select * from candidate_stack_assignment;
++----+----------------+--------------+---------------+---------------+--------+---------------------+--------------+
+| id | requirement_id | candidate_id | assigned_date | complete_date | status | creator_stamp       | creator_user |
++----+----------------+--------------+---------------+---------------+--------+---------------------+--------------+
+|  1 |              1 |            1 | 2001-01-19    | 2001-01-20    | active | 2003-04-05 01:00:02 | saI          |
+|  2 |              2 |            2 | 2002-01-19    | 2002-01-20    | active | 2005-04-05 01:00:02 | manoj        |
+|  3 |              3 |            3 | 2003-01-19    | 2003-01-20    | active | 2006-04-05 01:00:02 | vijay        |
+|  4 |              4 |            4 | 2004-01-19    | 2004-01-20    | active | 2007-04-05 01:00:02 | nithin       |
+|  5 |              5 |            5 | 2005-01-19    | 2005-01-20    | active | 2008-04-05 01:00:02 | vamsi        |
++----+----------------+--------------+---------------+---------------+--------+---------------------+--------------+
+5 rows in set (0.01 sec)
