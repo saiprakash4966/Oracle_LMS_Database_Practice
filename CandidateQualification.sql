fa2ce3a548cc0e56963945aa7ca26956f86af5e2
saiprakash4966 <97298373+saiprakash4966@users.noreply.github.com>
2022-08-28 19:44:12 +0530

    Update CandidateQualification.sql

--- CandidateQualification.sql
+++ CandidateQualification.sql
@@ -1,33 +1,9 @@
 
+mysql> create table Candidate_Qualification(Id int PRIMARY KEY,Candidate_Id int not null,Diploma VARCHAR(50) not null,degree_Name VARCHAR(50) not null,is_degreeName_Verified boolean,employee_descipline VARCHAR(50) not null,is_employee_descipline_verified boolean,passing_year datetime,is_Passingyear_Verified boolean,aggr_is_passingyear_verified boolean,final_year_per_is_final_year_per_verified boolean,training_institue VARCHAR(50) not null,is_training_institute_verified boolean,training_duration_month datetime,is_training_duration_month_verified boolean,other_training VARCHAR(50),is_Other_training_verified boolean,creator_stamp datetime,creator_user int not null, FOREIGN KEY (candidate_Id) REFERENCES fellowship_candidate(Id)
+    -> ON UPDATE CASCADE
+    -> ON DELETE CASCADE);
+Query OK, 0 rows affected (0.07 sec)
 
-mysql> create table Candidate_Qualification(Id int PRIMARY KEY,Candidate_Id int not null,Diploma VARCHAR(50) not null,degree_Name VARCHAR(50) not null,is_degreeName_Verified boolean,employee_descipline VARCHAR(50) not null,is_employee_descipline_verified boolean,passing_year datetime,is_Passingyear_Verified boolean,aggr_is_passingyear_verified boolean,final_year_per_is_final_year_per_verified boolean,training_institue VARCHAR(50) not null,is_training_institute_verified boolean,training_duration_month datetime,is_training_duration_month_verified boolean,other_training VARCHAR(50),is_Other_training_verified boolean,creator_stamp datetime,creator_user int not null);
-Query OK, 0 rows affected (0.05 sec)
-
-mysql> desc candidate_qualification;
-+-------------------------------------------+-------------+------+-----+---------+-------+
-| Field                                     | Type        | Null | Key | Default | Extra |
-+-------------------------------------------+-------------+------+-----+---------+-------+
-| Id                                        | int         | NO   | PRI | NULL    |       |
-| Candidate_Id                              | int         | NO   |     | NULL    |       |
-| Diploma                                   | varchar(50) | NO   |     | NULL    |       |
-| degree_Name                               | varchar(50) | NO   |     | NULL    |       |
-| is_degreeName_Verified                    | tinyint(1)  | YES  |     | NULL    |       |
-| employee_descipline                       | varchar(50) | NO   |     | NULL    |       |
-| is_employee_descipline_verified           | tinyint(1)  | YES  |     | NULL    |       |
-| passing_year                              | datetime    | YES  |     | NULL    |       |
-| is_Passingyear_Verified                   | tinyint(1)  | YES  |     | NULL    |       |
-| aggr_is_passingyear_verified              | tinyint(1)  | YES  |     | NULL    |       |
-| final_year_per_is_final_year_per_verified | tinyint(1)  | YES  |     | NULL    |       |
-| training_institue                         | varchar(50) | NO   |     | NULL    |       |
-| is_training_institute_verified            | tinyint(1)  | YES  |     | NULL    |       |
-| training_duration_month                   | datetime    | YES  |     | NULL    |       |
-| is_training_duration_month_verified       | tinyint(1)  | YES  |     | NULL    |       |
-| other_training                            | varchar(50) | YES  |     | NULL    |       |'
-| is_Other_training_verified                | tinyint(1)  | YES  |     | NULL    |       |
-| creator_stamp                             | datetime    | YES  |     | NULL    |       |
-| creator_user                              | int         | NO   |     | NULL    |       |
-+-------------------------------------------+-------------+------+-----+---------+-------+
-19 rows in set (0.00 sec)
 
 
 mysql> insert into candidate_qualification values(1,270,'Yes','No',1,'good',1,'06-01-10 09:08:07',1,1,1,'Qspider',1,'20-02-09 01:08:00',1,'No',1,'01-05-11 05:06:00',1);
